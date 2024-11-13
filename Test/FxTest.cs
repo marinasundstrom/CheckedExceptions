8f652c2f2a91caa1ba1b6125f4d1d99213fd53df
Marina <marna.li@outlook.com>
2024-11-13 12:11:53 +0100

    Update sample

--- Test/FxTest.cs
+++ Test/FxTest.cs
@@ -10,8 +10,22 @@ public class FxTest
         var x = int.Parse("f");
     }
 
-    [Throws(typeof(Exception))]
+    [Throws(typeof(ArgumentNullException))]
+    [Throws(typeof(OverflowException))]
     public void Foo2()
+    {
+        try
+        {
+            var x = int.Parse("f");
+        }
+        catch (FormatException)
+        {
+
+        }
+    }
+
+    [Throws(typeof(Exception))]
+    public void Foo3()
     {
         Foo();
     }
