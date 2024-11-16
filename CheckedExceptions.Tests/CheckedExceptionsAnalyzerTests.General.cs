3efbaad8baae8167b2295abadf52eedac7afee48
Marina <marna.li@outlook.com>
2024-11-16 19:55:51 +0100

    Add more tests

---
+++ CheckedExceptions.Tests/CheckedExceptionsAnalyzerTests.General.cs
@@ -0,0 +1,163 @@
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Testing;
+
+namespace CheckedExceptions.Test;
+
+using Verifier = CSharpAnalyzerVerifier<CheckedExceptionsAnalyzer, DefaultVerifier>;
+
+partial class CheckedExceptionsAnalyzerTests
+{
+    [Fact]
+    public async Task UncaughtExceptions_ShouldReportDiagnostic()
+    {
+        var test = @"
+using System;
+
+public class TestClass
+{
+    public void ThrowingMethod()
+    {
+        throw new InvalidOperationException();
+    }
+
+    public void CallerMethod()
+    {
+        ThrowingMethod(); // Diagnostic expected here
+    }
+}";
+
+        var expected = Verifier.Diagnostic("THROW001")
+            .WithSpan(8, 9, 8, 47)
+            .WithArguments("InvalidOperationException");
+
+        await Verifier.VerifyAnalyzerAsync(test, expected);
+    }
+
+    [Fact]
+    public async Task CaughtExceptions_ShouldNotReportDiagnostic()
+    {
+        var test = @"
+using System;
+
+public class TestClass
+{
+    public void ThrowingMethod()
+    {
+        throw new InvalidOperationException();
+    }
+
+    public void CallerMethod()
+    {
+        try
+        {
+            ThrowingMethod(); // Exception is caught
+        }
+        catch (InvalidOperationException)
+        {
+            // Exception handled
+        }
+    }
+}";
+
+        await Verifier.VerifyAnalyzerAsync(test);
+    }
+
+    [Fact]
+    public async Task DeclaringExceptions_ShouldNotReportDiagnostic()
+    {
+        var test = @"
+using System;
+
+public class TestClass
+{
+    public void ThrowingMethod()
+    {
+        throw new InvalidOperationException();
+    }
+
+    [Throws(typeof(InvalidOperationException))]
+    public void CallerMethod()
+    {
+        ThrowingMethod(); // Exception is declared
+    }
+}";
+
+        await Verifier.VerifyAnalyzerAsync(test);
+    }
+
+    [Fact]
+    public async Task MixedHandling_ShouldReportDiagnosticsForUnhandledExceptions()
+    {
+        var test = @"
+using System;
+
+public class TestClass
+{
+    public void ThrowingMethod1()
+    {
+        throw new InvalidOperationException();
+    }
+
+    public void ThrowingMethod2()
+    {
+        throw new ArgumentException();
+    }
+
+    public void CallerMethod()
+    {
+        try
+        {
+            ThrowingMethod1(); // Exception is caught
+        }
+        catch (InvalidOperationException)
+        {
+            // Exception handled
+        }
+
+        ThrowingMethod2(); // Diagnostic expected here
+    }
+}";
+
+        var expected1 = Verifier.Diagnostic("THROW001")
+            .WithSpan(8, 9, 8, 47)
+            .WithArguments("InvalidOperationException");
+
+        var expected2 = Verifier.Diagnostic("THROW001")
+            .WithSpan(13, 9, 13, 39)
+            .WithArguments("ArgumentException");
+
+        await Verifier.VerifyAnalyzerAsync(test, expected1, expected2);
+    }
+
+    [Fact]
+    public async Task DeclaringMultipleExceptions_ShouldNotReportDiagnostic()
+    {
+        var test = @"
+using System;
+
+public class TestClass
+{
+    public void ThrowingMethod1()
+    {
+        throw new InvalidOperationException();
+    }
+
+    public void ThrowingMethod2()
+    {
+        throw new ArgumentException();
+    }
+
+    [Throws(typeof(InvalidOperationException))]
+    [Throws(typeof(ArgumentException))]
+    public void CallerMethod()
+    {
+        ThrowingMethod1();
+        ThrowingMethod2();
+    }
+}";
+
+        await Verifier.VerifyAnalyzerAsync(test);
+    }
+
+
+}
