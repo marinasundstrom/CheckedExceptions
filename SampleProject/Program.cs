3bc2fa6fc56b040aaf8c6b6e6f770d0d0acd1f4d
Marina Sundström <marna.li@outlook.com>
2025-07-06 15:49:32 +0200

    Update sample

--- SampleProject/Program.cs
+++ SampleProject/Program.cs
@@ -1,59 +1,21 @@
-﻿Calculator calculator = new();
-
+﻿
 try
 {
-    Console.Write("Add integers\n");
+    var result = AddStringsAsNumbers("1", "b");
 
-    Console.Write("Left-hand side: ");
-    var lhsStr = Console.ReadLine();
-
-    var lhs = int.Parse(lhsStr!);
-
-    Console.Write("Right-hand side: ");
-    var rhsStr = Console.ReadLine();
-
-    var rhs = int.Parse(rhsStr!);
-
-    try
-    {
-        var sum = calculator.Add(lhs, rhs);
-
-        Console.WriteLine($"Sum is: {sum}");
-    }
-    catch (OverflowException e)
-    {
-        Console.WriteLine($"The sum exceeds the bounds of which min is {int.MinValue} and and max is {int.MaxValue}");
-    }
-}
-catch (OverflowException)
-{
-    Console.WriteLine($"Specified number must be between {int.MinValue} and {int.MaxValue}");
+    Console.WriteLine(result);
 }
 catch (FormatException)
 {
-    Console.WriteLine("Expected integer number");
+
 }
-catch (ArgumentOutOfRangeException)
+catch (OverflowException)
 {
-    Console.WriteLine("Argument is out of range");
-}
-catch
-{
-    Console.WriteLine("Out of memory");
+
 }
 
-public class Calculator
+[Throws(typeof(FormatException), typeof(OverflowException))]
+static int AddStringsAsNumbers(string a, string b)
 {
-    /// <summary>
-    /// Add numbers
-    /// </summary>
-    /// <param name="a">Left-hand side</param>
-    /// <param name="b">Right-hand side</param>
-    /// <returns>The sum of a and b.</returns>
-    /// <exception cref="System.OverflowException">The result exceeds the bounds.</exception>
-    [Throws(typeof(OverflowException))]
-    public int Add(int a, int b)
-    {
-        return a + b;
-    }
+    return int.Parse(a) + int.Parse(b);
 }
