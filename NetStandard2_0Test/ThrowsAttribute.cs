5166b647e21f7291365adfe6e8781d4d812a3f8b
Marina Sundström <marna.li@outlook.com>
2025-07-26 11:50:53 +0200

    Fix crash on netstandard2.0 #75

---
+++ NetStandard2_0Test/ThrowsAttribute.cs
@@ -0,0 +1,23 @@
+namespace System;
+
+[AttributeUsage(AttributeTargets.Method | AttributeTargets.Constructor | AttributeTargets.Delegate, AllowMultiple = true)]
+public class ThrowsAttribute : Attribute
+{
+    public List<Type> ExceptionTypes { get; } = new List<Type>();
+
+    public ThrowsAttribute(Type exceptionType, params Type[] exceptionTypes)
+    {
+        if (!typeof(Exception).IsAssignableFrom(exceptionType))
+            throw new ArgumentException("Must be an Exception type.");
+
+        ExceptionTypes.Add(exceptionType);
+
+        foreach (var type in exceptionTypes)
+        {
+            if (!typeof(Exception).IsAssignableFrom(type))
+                throw new ArgumentException("Must be an Exception type.");
+
+            ExceptionTypes.Add(type);
+        }
+    }
+}
