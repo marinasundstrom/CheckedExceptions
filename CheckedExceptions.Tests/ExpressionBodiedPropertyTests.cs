66dd5cb5ece99944c24c5c8704e44956c174221a
Marina Sundström <marna.li@outlook.com>
2025-07-28 15:56:52 +0200

    Disallow prop decl with Throws and Get accessor #145

--- CheckedExceptions.Tests/ExpressionBodiedPropertyTests.cs
+++ CheckedExceptions.Tests/ExpressionBodiedPropertyTests.cs
@@ -83,4 +83,38 @@ public partial class ExpressionBodiedPropertyTests
 
         await Verifier.VerifyAnalyzerAsync(test, expected, expected2);
     }
+
+    [Fact]
+    public async Task Invalid2()
+    {
+        var test = /* lang=c#-test */ """
+            using System;
+
+            public class TestClass
+            {
+                [Throws(typeof(InvalidOperationException))]
+                public int TestProp
+                {
+                    get
+                    {
+                        return Test(42);
+                    }
+                }
+
+                [Throws(typeof(InvalidOperationException))]
+                public int Test(int x)
+                {
+                    return 0;
+                }
+            }
+            """;
+
+        var expected = Verifier.UnhandledException("InvalidOperationException")
+            .WithSpan(10, 20, 10, 28);
+
+        var expected2 = Verifier.Diagnostic("THROW010")
+            .WithSpan(5, 6, 5, 47);
+
+        await Verifier.VerifyAnalyzerAsync(test, expected, expected2);
+    }
 }
