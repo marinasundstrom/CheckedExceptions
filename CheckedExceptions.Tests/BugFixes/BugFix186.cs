d9ea1756f11c320d206e8c2534c3972c58d923d8
Marina Sundström <marna.li@outlook.com>
2025-08-01 22:46:05 +0200

    Add and update unit tests

--- CheckedExceptions.Tests/BugFixes/BugFix186.cs
+++ CheckedExceptions.Tests/BugFixes/BugFix186.cs
@@ -10,10 +10,56 @@ public class BugFix186
     public async Task Test1()
     {
         var test = /* lang=c#-test */ """
+        using System;
+
         public class TestBase
         {
             [Throws(typeof(ArgumentNullException))]
             public virtual bool Foo3 { get; set; }
+        }
+
+        public class TestDerive : TestBase
+        {
+            public override bool Foo3
+            {
+                [Throws(typeof(ArgumentNullException))]
+                get => throw new ArgumentNullException();
+            }
+        }
+        """;
+
+
+        var expected = Verifier.Diagnostic(CheckedExceptionsAnalyzer.DiagnosticIdThrowsDeclarationNotValidOnFullProperty)
+            .WithSpan(5, 6, 5, 43);
+
+        var expected2 = Verifier.Diagnostic(CheckedExceptionsAnalyzer.DiagnosticIdMissingThrowsOnBaseMember)
+            .WithArguments("TestBase.get_Foo3()", "ArgumentNullException")
+            .WithSpan(13, 24, 13, 45);
+
+        await Verifier.VerifyAnalyzerAsync(test, s =>
+        {
+            s.ExpectedDiagnostics.AddRange(expected, expected2);
+
+            s.DisabledDiagnostics.Remove(CheckedExceptionsAnalyzer.DiagnosticIdRedundantExceptionDeclaration);
+
+            s.DisabledDiagnostics.Add(CheckedExceptionsAnalyzer.DiagnosticIdGeneralThrows);
+        });
+    }
+
+    [Fact]
+    public async Task Test2()
+    {
+        var test = /* lang=c#-test */ """
+        using System;
+
+        public class TestBase
+        {
+            public virtual bool Foo3 
+            {
+                [Throws(typeof(ArgumentNullException))] 
+                get; 
+                set; 
+            }
         }
 
         public class TestDerive : TestBase
@@ -35,54 +81,43 @@ public class BugFix186
     }
 
     [Fact]
-    public async Task Test2()
+    public async Task Test3()
     {
         var test = /* lang=c#-test */ """
+        using System;
+
         public class TestBase
         {
-            [Throws(typeof(ArgumentNullException))]
-            public virtual bool Foo3 { get; set; }
+            public virtual bool Foo3 
+            {
+                [Throws(typeof(ArgumentNullException))] 
+                get; 
+                set; 
+            }
         }
 
         public class TestDerive : TestBase
         {
             public override bool Foo3
             {
-                get => throw new ArgumentNullException();
+                [Throws(typeof(InvalidOperationException))]
+                get => throw new InvalidOperationException();
             }
         }
         """;
 
-        await Verifier.VerifyAnalyzerAsync(test, s =>
-        {
-            s.DisabledDiagnostics.Remove(CheckedExceptionsAnalyzer.DiagnosticIdRedundantExceptionDeclaration);
+        var expected = Verifier.Diagnostic(CheckedExceptionsAnalyzer.DiagnosticIdMissingThrowsOnBaseMember)
+            .WithArguments("TestBase.get_Foo3()", "InvalidOperationException")
+            .WithSpan(17, 24, 17, 49);
 
-            s.DisabledDiagnostics.Add(CheckedExceptionsAnalyzer.DiagnosticIdGeneralThrows);
-        });
-    }
-    
-    [Fact]
-    public async Task Test3()
-    {
-        var test = /* lang=c#-test */ """
-                                      public class TestBase1
-                                      {
-                                          [Throws(typeof(ArgumentNullException))]
-                                          public virtual bool Foo3 { get; set; }
-                                      }
-                                      
-                                      public class TestDerive1 : TestBase1
-                                      {
-                                          public override bool Foo3
-                                          {
-                                              [Throws(typeof(ArgumentNullException))]
-                                              get => throw new ArgumentNullException();
-                                          }
-                                      }
-                                      """;
+        var expected2 = Verifier.Diagnostic(CheckedExceptionsAnalyzer.DiagnosticIdMissingThrowsFromBaseMember)
+            .WithArguments("TestBase.get_Foo3()", "ArgumentNullException")
+            .WithSpan(18, 9, 18, 12);
 
         await Verifier.VerifyAnalyzerAsync(test, s =>
         {
+            s.ExpectedDiagnostics.AddRange(expected, expected2);
+
             s.DisabledDiagnostics.Remove(CheckedExceptionsAnalyzer.DiagnosticIdRedundantExceptionDeclaration);
 
             s.DisabledDiagnostics.Add(CheckedExceptionsAnalyzer.DiagnosticIdGeneralThrows);
