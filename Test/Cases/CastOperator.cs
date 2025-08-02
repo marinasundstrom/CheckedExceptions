4e6ab2e2f8f3f71fb0e48e1060674d12f38dd0ed
Marina Sundström <marna.li@outlook.com>
2025-08-02 12:04:52 +0200

    Fix test code

--- Test/Cases/CastOperator.cs
+++ Test/Cases/CastOperator.cs
@@ -16,7 +16,7 @@ public class CastOperator
             long o = 20;
             var x3 = (int)o;
 
-            var x4 = (int)1e100;
+            //var x4 = (int)1e100;
             var x5 = (int)42.2;
         }
     }
