3f21295db8a74c88d69e655042ca20809d9e9bf7
Marina Sundström <marna.li@outlook.com>
2025-08-02 13:06:23 +0200

    Remove try when removing last catch using codefix #198 (#205)

--- CheckedExceptions.Tests/CodeFixes/RemoveRedundantCatchClauseCodeFixProviderTests.cs
+++ CheckedExceptions.Tests/CodeFixes/RemoveRedundantCatchClauseCodeFixProviderTests.cs
@@ -47,9 +47,93 @@ catch (OverflowException overflowException)
 }
 """;
 
-        var expectedDiagnostic = Verifier.Diagnostic("THROW009")
+        var expectedDiagnostic = Verifier.Diagnostic(CheckedExceptionsAnalyzer.DiagnosticIdRedundantTypedCatchClause)
                 .WithArguments("ArgumentException")
                 .WithSpan(14, 8, 14, 25);
+
+
+        await Verifier.VerifyCodeFixAsync(testCode, [expectedDiagnostic], fixedCode, executable: true, setup: option =>
+        {
+            option.DisabledDiagnostics.Remove(CheckedExceptionsAnalyzer.DiagnosticIdRedundantTypedCatchClause);
+        });
+    }
+
+    [Fact]
+    public async Task RemoveSingleCatchBlock_RemoveTry()
+    {
+        var testCode = /* lang=c#-test */  """
+#nullable enable
+using System;
+
+public class Foo 
+{
+    public void Test() 
+    {
+        try
+        {
+            int x = 0;
+            string z = "";
+        }
+        catch (FormatException formatException)
+        {
+        }
+    }
+}
+""";
+
+        var fixedCode = /* lang=c#-test */  """
+#nullable enable
+using System;
+
+public class Foo 
+{
+    public void Test() 
+    {
+        int x = 0;
+        string z = "";
+    }
+}
+""";
+
+        var expectedDiagnostic = Verifier.Diagnostic(CheckedExceptionsAnalyzer.DiagnosticIdRedundantTypedCatchClause)
+                .WithArguments("FormatException")
+                .WithSpan(13, 16, 13, 31);
+
+
+        await Verifier.VerifyCodeFixAsync(testCode, [expectedDiagnostic], fixedCode, setup: option =>
+        {
+            option.DisabledDiagnostics.Remove(CheckedExceptionsAnalyzer.DiagnosticIdRedundantTypedCatchClause);
+        });
+    }
+
+    [Fact]
+    public async Task RemoveSingleCatchBlock_RemoveTry_GlobalStatement()
+    {
+        var testCode = /* lang=c#-test */  """
+#nullable enable
+using System;
+
+try
+{
+    int x = 0;
+    string z = "";
+}
+catch (FormatException formatException)
+{
+}
+""";
+
+        var fixedCode = /* lang=c#-test */  """
+#nullable enable
+using System;
+
+int x = 0;
+string z = "";
+""";
+
+        var expectedDiagnostic = Verifier.Diagnostic(CheckedExceptionsAnalyzer.DiagnosticIdRedundantTypedCatchClause)
+                .WithArguments("FormatException")
+                .WithSpan(9, 8, 9, 23);
 
 
         await Verifier.VerifyCodeFixAsync(testCode, [expectedDiagnostic], fixedCode, executable: true, setup: option =>
