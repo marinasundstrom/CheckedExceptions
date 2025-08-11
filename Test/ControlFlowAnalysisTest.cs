9d9812e09140f8ff678edb73a962d9e54d6d713a
Marina Sundström <marna.li@outlook.com>
2025-08-11 20:43:09 +0200

    Add sample

---
+++ Test/ControlFlowAnalysisTest.cs
@@ -0,0 +1,30 @@
+namespace Test;
+
+public class ControlFlowAnalysisTest
+{
+    public int Test()
+    {
+        try
+        {
+            return int.Parse("foo");
+        }
+        catch (FormatException ex)
+        {
+            return -1;
+        }
+        catch (OverflowException ex)
+        {
+            return -1;
+        }
+        catch (InvalidOperationException ex)
+        {
+            return -1;
+        }
+        catch
+        {
+            return -1;
+        }
+
+        Console.WriteLine("Test");
+    }
+}
